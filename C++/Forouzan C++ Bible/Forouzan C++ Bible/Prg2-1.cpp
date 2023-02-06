775b9550b288a6ef1b21e07fb328ea077651b243
butter3415 <butter3415@gmail.com>
2023-02-06 22:15:09 +0900

    C++ Start

---
+++ C++/Forouzan C++ Bible/Forouzan C++ Bible/Prg2-1.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+
+int main()  
+{
+	std::cout << "안녕하세요? C++ 설치만 몇주동안 했습니다^^" << std::endl ;
+	std::cout << "이쯤되면 설치가 될 만도 한데^^" << std::endl;
+	std::cout << "잘 된걸까요? 이제부터 열심히 해보겠습니다^^" << std::endl;
+	return 0;
+}
+
+/*
+1 - # include 전처리 지시자 : 컴파일러에게 추가적인 지시를 내리기 위해 사용하는 구문
+    <iostream> : input/output stream
+2 - main(){} : c++ 은 main 함수에서 시작해서 main 함수로 끝나야한다.
+			   중괄호를 열었으면 꼭 닫아야함.
+3 - std::cout : C++ 객체의 성(std - standard)과 이름(cout - consoleout) / std 는 객체가 속한 그룹을 나타냄
+4 - std::endl : endl-endline (줄이 끝났다)
+*/
---
+++ C++/StartCode/StartCode/text.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+void main() 
+{
+	cout << "Hello World" << endl;
+	cout << "안녕하세요~ 반가워요~! 잘있어요~ 다시 만나요~";
+}
