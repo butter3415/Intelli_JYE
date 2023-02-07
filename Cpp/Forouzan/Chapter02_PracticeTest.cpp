fdac58e44faaba12b8140258b83f84e674a04f2c
butter3415 <butter3415@gmail.com>
2023-02-08 00:20:46 +0900

    Chapter02 연습문제 리뷰

---
+++ Cpp/Forouzan/Chapter02_PracticeTest.cpp
@@ -0,0 +1,57 @@
+//
+// Created by 82104 on 2023-02-08.
+//
+
+# include <iostream>
+using namespace std;
+
+int main()
+{
+//    // 01
+//    int first = 26;
+//    cout << 25 << endl;
+//    cout << first;  // first 를 출력하려면 먼저 정의해주어야함.
+
+//    // 02 - 03
+//    cout << 35 << endl;
+//    cout << 45;
+//    // std::cout << 35 << std::endl; -> std:: 생략 위해선 using namespace std; 작성해주어야 함.
+//    // 반대로 using namespace std; 작성하였으면 std:: 생략해야함.
+
+//    // 05
+//    int a = 25;
+//    // a + 30 = a;
+//    a = a + 30;     // 변수가 오른쪽, 값이 왼쪽에 와야함.
+//    cout << a << endl;
+
+//    // 06
+//    string name;
+//    cout << name;
+//    cin >> name;
+//    // string 타입 문자열을 출력하려면 입력을 받거나 초기화를 시켜주어야 한다.
+
+//    // 07
+//    int y = 32.34;
+//    cout << y;      // 소수점 버림하여 y 출력 시 32 만 출력된다.
+
+//    // 08
+//    float y = 32.34;
+//    cout << y;      // float 타입이기 때문에 소숫점까지 모두 출력된다.
+
+//    // 09
+//    double y = 32.34;
+//    cout << y ;     // 출력할 대상을 작성해야 한다.
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
