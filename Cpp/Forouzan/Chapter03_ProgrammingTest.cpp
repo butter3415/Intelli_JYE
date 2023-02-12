98d81ca38cb4bd0fd3dfea11aaa2ca229bd009dc
butter3415 <butter3415@gmail.com>
2023-02-12 19:34:06 +0900

    Chapter03 프로그래밍 문제 - 4-5

--- Cpp/Forouzan/Chapter03_ProgrammingTest.cpp
+++ Cpp/Forouzan/Chapter03_ProgrammingTest.cpp
@@ -7,44 +7,72 @@
 using namespace std;
 
 int main(){
-    // * 01. short 자료형과 unsigned int 자료형의 최대값과 최소값을 찾는 프로그램을 작성하시오.
-    // * 02. long 자료형과 long long 자료형의 최대값과 최소값을 찾는 프로그램을 작성하시오.
-    // * 03. float 자료형과 double 자료형의 최대값과 최소값을 찾는 프로그램을 작성하시오.
+//// * 01. short 자료형과 unsigned int 자료형의 최대값과 최소값을 찾는 프로그램을 작성하시오.
+//// * 02. long 자료형과 long long 자료형의 최대값과 최소값을 찾는 프로그램을 작성하시오.
+//// * 03. float 자료형과 double 자료형의 최대값과 최소값을 찾는 프로그램을 작성하시오.
+//
+//    unsigned int maxI = numeric_limits<unsigned int>::max();
+//    unsigned int minI = numeric_limits<unsigned int>::min();
+//
+//    cout << "int형의 최대값 : " << maxI << endl;
+//    cout << "int형의 최소값 : " << minI << endl;
+//
+//    short maxS = numeric_limits<short>::max();
+//    short minS = numeric_limits<short>::min();
+//
+//    cout << "short형의 최대값 : " << maxS << endl;
+//    cout << "short형의 최소값 : " << minS << endl;
+//
+//    long maxl = numeric_limits<long>::max();
+//    long minl = numeric_limits<long>::min();
+//
+//    cout << "long형의 최대값 : " << maxl << endl;
+//    cout << "long형의 최소값 : " << minl << endl;
+//
+//    long long maxll = numeric_limits<long long>::max();
+//    long long minll = numeric_limits<long long>::min();
+//
+//    cout << "long long형의 최대값 : " << maxll << endl;
+//    cout << "long long형의 최소값 : " << minll << endl;
+//
+//    float maxf = numeric_limits<float>::max();
+//    float minf = numeric_limits<float>::min();
+//
+//    cout << "float형의 최대값 : " << maxf << endl;
+//    cout << "float형의 최소값 : " << minf << endl;
+//
+//    double maxd = numeric_limits<double>::max();
+//    double mind = numeric_limits<double>::min();
+//
+//    cout << "double형의 최대값 : " << maxd << endl;
+//    cout << "double형의 최소값 : " << mind << endl;
 
-    unsigned int maxI = numeric_limits<unsigned int>::max();
-    unsigned int minI = numeric_limits<unsigned int>::min();
+//// 04. int 자료형의 정수를 입력받아서, 두 번째 자릿수를 추출한 뒤 출력하는 프로그램을 만드세요.
+//    int number, secondNum;
+//
+//    cout << "int 자료형의 정수 입력 :";
+//    cin >> number;
+//
+//    secondNum = (number % 100) / 10 ;
+//
+//    cout << "정수의 두 번째 자릿수 : " << secondNum << endl ;
 
-    cout << "int형의 최대값 : " << maxI << endl;
-    cout << "int형의 최소값 : " << minI << endl;
+// 05. int 자료형의 정수를 입력받아서, 첫 번째부터 세 번째 자릿수를 각각 출력하는 프로그램을 만드세요.
+    int number, firstNum, secondNum, thirdNum;
 
-    short maxS = numeric_limits<short>::max();
-    short minS = numeric_limits<short>::min();
+    cout << "int 자료형 정수 입력 :";
+    cin >> number;
 
-    cout << "short형의 최대값 : " << maxS << endl;
-    cout << "short형의 최소값 : " << minS << endl;
+    firstNum = number % 10;
+    secondNum = (number % 100) / 10 ;
+    thirdNum = (number % 1000) / 100 ;
 
-    long maxl = numeric_limits<long>::max();
-    long minl = numeric_limits<long>::min();
+    cout << "입력한 정수 : " << number << endl;
+    cout << "첫번째 자릿수 : " << firstNum << endl;
+    cout << "두번째 자릿수 : " << secondNum << endl;
+    cout << "세번째 자릿수 : " << thirdNum << endl;
 
-    cout << "long형의 최대값 : " << maxl << endl;
-    cout << "long형의 최소값 : " << minl << endl;
 
-    long long maxll = numeric_limits<long long>::max();
-    long long minll = numeric_limits<long long>::min();
 
-    cout << "long long형의 최대값 : " << maxll << endl;
-    cout << "long long형의 최소값 : " << minll << endl;
-
-    float maxf = numeric_limits<float>::max();
-    float minf = numeric_limits<float>::min();
-
-    cout << "float형의 최대값 : " << maxf << endl;
-    cout << "float형의 최소값 : " << minf << endl;
-
-    double maxd = numeric_limits<double>::max();
-    double mind = numeric_limits<double>::min();
-
-    cout << "double형의 최대값 : " << maxd << endl;
-    cout << "double형의 최소값 : " << mind << endl;
 
 }
