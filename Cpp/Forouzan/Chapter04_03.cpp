9eb3a955521d76444bc4952ddc8ebca32689ca7d
butter3415 <butter3415@gmail.com>
2023-02-16 00:06:41 +0900

    Chapter04 조건문 - switch 조건문과 default 분기

--- Cpp/Forouzan/Chapter04_03.cpp
+++ Cpp/Forouzan/Chapter04_03.cpp
@@ -31,34 +31,72 @@ int main() {
     // 정수 4 입력 시 case 4 부분으로 진입하고 나머지 문장 모두 실행함.
 // 어떤 case 분기와도 맞지 않으면 어떤 문장도 실행하지 않음.
 
-// break 구문
-// : switch 조건문을 여러 방향 조건 분기에 사용하려면 break 구문을 사용해야 한다.
-// : break 구문은 프로그램의 흐름을 중지하고 switch 조건문을 끝마치게 하는 구문
+//// break 구문
+//// : switch 조건문을 여러 방향 조건 분기에 사용하려면 break 구문을 사용해야 한다.
+//// : break 구문은 프로그램의 흐름을 중지하고 switch 조건문을 끝마치게 하는 구문
+//
+//// * switch 조건문과 break 구문을 조합해서 요일을 출력하는 프로그램
+//    int day;
+//
+//    cout << "0~6 까지의 정수 입력 :" ;
+//    cin >> day;
+//
+//    switch (day){
+//        case 0: cout << "일요일" << endl;
+//                cout << "한주의 시작!" << endl;
+//                break;
+//        case 1: cout << "월요일" << endl;
+//            break;
+//        case 2: cout << "화요일" << endl;
+//            break;
+//        case 3: cout << "수요일" << endl;
+//            break;
+//        case 4: cout << "목요일" << endl;
+//            break;
+//        case 5: cout << "금요일" << endl;
+//            break;
+//        case 6: cout << "토요일" << endl;
+//                cout << "한주의 마지막!" << endl;
+//            break;
+//    }
 
-// * switch 조건문과 break 구문을 조합해서 요일을 출력하는 프로그램
-    int day;
+// - default 분기
+//   : switch 구문의 모든 case 분기에도 진입하지 못 한 경우 특정 문장을 실행하고 싶을때 default 구문 실행
 
-    cout << "0~6 까지의 정수 입력 :" ;
-    cin >> day;
+// * switch 조건을 사용해서 점수를 기반으로 학접 출력하기
+    int score;
+    char grade;
 
-    switch (day){
-        case 0: cout << "일요일" << endl;
-                cout << "한주의 시작!" << endl;
-                break;
-        case 1: cout << "월요일" << endl;
-            break;
-        case 2: cout << "화요일" << endl;
-            break;
-        case 3: cout << "수요일" << endl;
-            break;
-        case 4: cout << "목요일" << endl;
-            break;
-        case 5: cout << "금요일" << endl;
-            break;
-        case 6: cout << "토요일" << endl;
-                cout << "한주의 마지막!" << endl;
-            break;
+    cout << "점수(1~100) :";
+    cin >> score;
+
+    if (score > 100){
+        score = 100;
+    } else if(score < 0){
+        score = 0;
     }
+
+    switch(score /10){
+        case 10 : grade = 'A';
+                  break;
+        case 9 : grade = 'A';
+                 break;
+        case 8 : grade = 'B';
+                 break;
+        case 7 : grade = 'C';
+                 break;
+        case 6 : grade = 'D';
+                 break;
+        default : grade = 'F';
+    }
+
+    cout << "점수는 " << score << "점이며, " ;
+    cout << "학점은 " << grade << "입니다!" << endl;
+
+
+
+
+
 
 
 
