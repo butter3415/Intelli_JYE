29174f7e97fc7df440ff01de2e22afc59d6b9323
butter3415 <butter3415@gmail.com>
2023-02-19 17:30:50 +0900

    Chapter04 조건문 - 프로그래밍 문제 02

--- Cpp/Forouzan/Chapter04_ProgrammingTest.cpp
+++ Cpp/Forouzan/Chapter04_ProgrammingTest.cpp
@@ -19,6 +19,32 @@ int main() {
 //
 //    cout << "자릿수 값을 바꾼 정수 : " << changeNum ;
 
+// * 02. 3개의 정수를 입력받고, 가장 작은 것을 출력하는 프로그램을 만드세요.
+    int num1, num2, num3, minNum;
+
+    cout << "첫 번째 정수 :";
+    cin >> num1;
+    cout << "두 번째 정수 :";
+    cin >> num2;
+    cout << "세 번째 정수 :";
+    cin >> num3;
+
+    if ((num1 < num2) && (num1 < num3)){
+        minNum = num1;
+        cout << "3개의 값 중 가장 작은 수는 " << minNum << "이다." << endl;
+    } else if ((num2 < num1) && (num2 < num3)){
+        minNum = num2;
+        cout << "3개의 값 중 가장 작은 수는 " << minNum << "이다." << endl;
+    } else if ((num3 < num1) && (num3 < num2)){
+        minNum = num3;
+        cout << "3개의 값 중 가장 작은 수는 " << minNum << "이다." << endl;
+    } else {
+        cout << "값을 다시 입력하세요.";
+    }
+
+
+
+
 
 
 
