9f5c8f11e94fe4c2872b8b7130490f8f1bc2c116
butter3415 <butter3415@gmail.com>
2023-02-19 17:25:11 +0900

    Chapter04 조건문 - 프로그래밍 문제 01

---
+++ Cpp/Forouzan/Chapter04_ProgrammingTest.cpp
@@ -0,0 +1,27 @@
+// 04 조건문
+// Created by 82104 on 2023-02-19.
+// 04. 프로그래밍 문제
+
+# include <iostream>
+using namespace std;
+
+int main() {
+//// *01. 사용자로부터 부호 없는 두 자리 정수를 입력받고, 자리수의 값을 바꿔서 출력하는 프로그램을 만드시오.
+//    int number, firstNum, secondNum, changeNum;
+//
+//    cout << "두 자리 정수 입력 :";
+//    cin >> number;
+//
+//    firstNum = number / 10;
+//    secondNum = number % 10;
+//
+//    changeNum = (secondNum * 10) + (firstNum * 1);
+//
+//    cout << "자릿수 값을 바꾼 정수 : " << changeNum ;
+
+
+
+
+
+
+}
